44eb9ba0d2f67f595d376d1da127d23ad91cd59d
root <danma@yunify.com>
2021-03-04 15:31:26 +0800

    Update edge join API path and response

--- pkg/edgeservice/handler.go
+++ pkg/edgeservice/handler.go
@@ -14,7 +14,8 @@ limitations under the License.
 package edgeservice
 
 import (
-	//"encoding/base64"
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
@@ -70,6 +71,18 @@ type CloudStream struct {
 	TunnelPort uint32 `yaml:"tunnelPort,omitempty"`
 }
 
+type EdgeJoinResponse struct {
+	Code    uint32 `json:"code,omitempty"`
+	Status  string `json:"status,omitempty"`
+	Message string `json:"message,omitempty"`
+	Data    string `json:"data,omitempty"`
+}
+
+const (
+	StatusSucceeded = "Succeeded"
+	StatusFailure   = "Failure"
+)
+
 var k8sClient *kubernetes.Clientset
 
 func InitK8sClient(kubeconfig string) {
@@ -100,29 +113,42 @@ func EdgeNodeJoin(request *restful.Request, response *restful.Response) {
 	//Validate Node name
 	msgs := validation.NameIsDNSSubdomain(nodeName, false)
 	if len(msgs) != 0 {
-		log.Printf("Invalid node name: %s\n", msgs[0])
-		response.AddHeader("Content-Type", "text/plain")
-		errMsg := fmt.Sprintf("Invalid node name: %s", msgs[0])
-		response.WriteErrorString(http.StatusBadRequest, errMsg)
+		log.Printf("EdgeNodeJoin: Invalid node name: %s\n", msgs[0])
+		response.AddHeader("Content-Type", "text/json")
+		response.WriteHeader(http.StatusBadRequest)
+		response.WriteAsJson(&EdgeJoinResponse{
+			Code:    http.StatusBadRequest,
+			Status:  StatusFailure,
+			Message: fmt.Sprintf("Invalid node name: %s", msgs[0]),
+		})
 		return
 	}
 
 	//Validate IP address
 	ip := net.ParseIP(nodeIP)
 	if ip == nil {
-		log.Printf("Invalid node IP: %s\n", nodeIP)
-		response.AddHeader("Content-Type", "text/plain")
-		errMsg := fmt.Sprintf("Invalid node IP: %s", nodeIP)
-		response.WriteErrorString(http.StatusBadRequest, errMsg)
+		log.Printf("EdgeNodeJoin: Invalid node IP: %s\n", nodeIP)
+		response.AddHeader("Content-Type", "text/json")
+		response.WriteHeader(http.StatusBadRequest)
+		response.WriteAsJson(&EdgeJoinResponse{
+			Code:    http.StatusBadRequest,
+			Status:  StatusFailure,
+			Message: fmt.Sprintf("Invalid node IP: %s", nodeIP),
+		})
 		return
 	}
 
 	//Check Node name and IP used
 	nodeList, err := k8sClient.CoreV1().Nodes().List(metav1.ListOptions{})
 	if err != nil {
-		log.Printf("List nodes error [+%v]\n", err)
-		response.AddHeader("Content-Type", "text/plain")
-		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+		log.Printf("EdgeNodeJoin: List nodes error [+%v]\n", err)
+		response.AddHeader("Content-Type", "text/json")
+		response.WriteHeader(http.StatusInternalServerError)
+		response.WriteAsJson(&EdgeJoinResponse{
+			Code:    http.StatusInternalServerError,
+			Status:  StatusFailure,
+			Message: fmt.Sprintf("List nodes error [+%v]", err),
+		})
 		return
 	}
 
@@ -136,44 +162,67 @@ func EdgeNodeJoin(request *restful.Request, response *restful.Response) {
 
 	_, ok := nodeNames[nodeName]
 	if ok {
-		log.Printf("Node name %s already used\n", nodeName)
-		response.AddHeader("Content-Type", "text/plain")
-		errMsg := fmt.Sprintf("Node name %s already used\n", nodeName)
-		response.WriteErrorString(http.StatusBadRequest, errMsg)
+		log.Printf("EdgeNodeJoin: Node name %s in use\n", nodeName)
+		response.AddHeader("Content-Type", "text/json")
+		response.WriteHeader(http.StatusBadRequest)
+		response.WriteAsJson(&EdgeJoinResponse{
+			Code:    http.StatusBadRequest,
+			Status:  StatusFailure,
+			Message: fmt.Sprintf("Node name %s in use", nodeName),
+		})
 		return
 	}
 
 	_, ok = nodeIPs[nodeIP]
 	if ok {
-		log.Printf("Node IP %s already used\n", nodeIP)
-		response.AddHeader("Content-Type", "text/plain")
-		errMsg := fmt.Sprintf("Node IP %s already used\n", nodeIP)
-		response.WriteErrorString(http.StatusBadRequest, errMsg)
+		log.Printf("EdgeNodeJoin: Node IP %s in use\n", nodeIP)
+		response.AddHeader("Content-Type", "text/json")
+		response.WriteHeader(http.StatusBadRequest)
+		response.WriteAsJson(&EdgeJoinResponse{
+			Code:    http.StatusBadRequest,
+			Status:  StatusFailure,
+			Message: fmt.Sprintf("Node IP %s in use", nodeIP),
+		})
 		return
 	}
 
 	configMap, err := k8sClient.CoreV1().ConfigMaps(KubeEdgeNamespace).Get(KubeEdgeCloudCoreConfigName, metav1.GetOptions{})
 	if err != nil {
-		log.Printf("Read cloudcore configmap error [+%v]\n", err)
-		response.AddHeader("Content-Type", "text/plain")
-		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+		log.Printf("EdgeNodeJoin: Read cloudcore configmap error [+%v]\n", err)
+		response.AddHeader("Content-Type", "text/json")
+		response.WriteHeader(http.StatusInternalServerError)
+		response.WriteAsJson(&EdgeJoinResponse{
+			Code:    http.StatusInternalServerError,
+			Status:  StatusFailure,
+			Message: fmt.Sprintf("Read cloudcore configmap error [+%v]", err),
+		})
 		return
 	}
 
 	var cloudCoreConfig CloudCoreConfig
 	err = yaml.Unmarshal([]byte(configMap.Data["cloudcore.yaml"]), &cloudCoreConfig)
 	if err != nil {
-		log.Printf("Unmarshal cloudcore configmap error [+%v]\n", err)
-		response.AddHeader("Content-Type", "text/plain")
-		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+		log.Printf("EdgeNodeJoin: Unmarshal cloudcore configmap error [+%v]\n", err)
+		response.AddHeader("Content-Type", "text/json")
+		response.WriteHeader(http.StatusInternalServerError)
+		response.WriteAsJson(&EdgeJoinResponse{
+			Code:    http.StatusInternalServerError,
+			Status:  StatusFailure,
+			Message: fmt.Sprintf("Unmarshal cloudcore configmap error [+%v]", err),
+		})
 		return
 	}
 
 	secret, err := k8sClient.CoreV1().Secrets(KubeEdgeNamespace).Get(KubeEdgeTokenSecretName, metav1.GetOptions{})
 	if err != nil {
-		log.Printf("Read cloudcore token secret error [+%v]\n", err)
-		response.AddHeader("Content-Type", "text/plain")
-		response.WriteErrorString(http.StatusInternalServerError, err.Error())
+		log.Printf("EdgeNodeJoin: Read cloudcore token secret error [+%v]\n", err)
+		response.AddHeader("Content-Type", "text/json")
+		response.WriteHeader(http.StatusInternalServerError)
+		response.WriteAsJson(&EdgeJoinResponse{
+			Code:    http.StatusInternalServerError,
+			Status:  StatusFailure,
+			Message: fmt.Sprintf("Read cloudcore token secret error [+%v]", err),
+		})
 		return
 	}
 
@@ -184,7 +233,17 @@ func EdgeNodeJoin(request *restful.Request, response *restful.Response) {
 	certPort := modules.CloudHub.HTTPS.Port
 	tunnelPort := modules.CloudStream.TunnelPort
 
-	cmd := fmt.Sprintf("arch=$(uname -m) curl -O https://kubeedge.pek3b.qingstor.com/bin/v1.5.0/$arch/keadm && chmod +x keadm && ./keadm join --kubeedge-version=1.5.0 --cloudcore-ipport=%s:%d --quicport %d --certport %d --tunnelport %d --edgenode-name %s --edgenode-ip %s --token %s", advertiseAddress, webSocketPort, quicPort, certPort, tunnelPort, nodeName, nodeIP, string(secret.Data["tokendata"]))
+	resp := EdgeJoinResponse{
+		Code:   http.StatusOK,
+		Status: StatusSucceeded,
+		Data:   fmt.Sprintf("arch=$(uname -m) curl -O https://kubeedge.pek3b.qingstor.com/bin/v1.5.0/$arch/keadm && chmod +x keadm && ./keadm join --kubeedge-version=1.5.0 --cloudcore-ipport=%s:%d --quicport %d --certport %d --tunnelport %d --edgenode-name %s --edgenode-ip %s --token %s", advertiseAddress, webSocketPort, quicPort, certPort, tunnelPort, nodeName, nodeIP, string(secret.Data["tokendata"])),
+	}
+	bf := bytes.NewBufferString("")
+	jsonEncoder := json.NewEncoder(bf)
+	jsonEncoder.SetEscapeHTML(false)
+	jsonEncoder.Encode(resp)
 
-	response.WriteHeaderAndEntity(http.StatusOK, cmd)
+	response.AddHeader("Content-Type", "text/json")
+	response.WriteHeader(http.StatusOK)
+	response.Write(bf.Bytes())
 }
